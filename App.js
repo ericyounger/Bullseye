f878adea0e215a0df135ff8899cc65f303c5fa8f
ericyounger <ericy@stud.ntnu.no>
2021-02-25 22:13:39 +0100

    added prettier config file

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
-	StyleSheet,
-	Text,
-	View,
-	TouchableOpacity,
-	ImageBackground,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    ImageBackground,
 } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
@@ -27,193 +27,193 @@ import InGame from "./pages/inGame/InGame";
 import PreviousGame from "./pages/previousGame/PreviousGame";
 
 const MyTheme = {
-	dark: true,
-	colors: {
-		primary: "rgb(255, 45, 85)",
-		background: "transparent",
-		card: "rgb(255, 255, 255)",
-		text: "#ffffff",
-		border: "rgb(199, 199, 204)",
-		notification: "rgb(255, 69, 58)",
-	},
+    dark: true,
+    colors: {
+        primary: "rgb(255, 45, 85)",
+        background: "transparent",
+        card: "rgb(255, 255, 255)",
+        text: "#ffffff",
+        border: "rgb(199, 199, 204)",
+        notification: "rgb(255, 69, 58)",
+    },
 };
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-	const [loggedIn, setLoggedIn] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(true);
 
-	if (!loggedIn) {
-		return (
-			<ImageBackground
-				source={require("./assets/blurBackground.jpg")}
-				style={styles.image}
-			>
-				<NavigationContainer theme={MyTheme}>
-					<Tab.Navigator>
-						<Tab.Screen
-							name="Login"
-							component={Login}
-							options={({ route }) => ({
-								tabBarVisible: false,
-							})}
-						/>
-						<Tab.Screen
-							name="Register"
-							component={Register}
-							options={({ route }) => ({
-								tabBarVisible: false,
-							})}
-						/>
-					</Tab.Navigator>
-					<StatusBar style="auto" />
-				</NavigationContainer>
-			</ImageBackground>
-		);
-	} else {
-		return (
-			<ImageBackground
-				source={require("./assets/blurBackground.jpg")}
-				style={styles.image}
-			>
-				<NavigationContainer theme={MyTheme}>
-					<Tab.Navigator tabBar={(props) => <Menu {...props} />}>
-						<Tab.Screen name="Home" component={HomeScreen} />
-						<Tab.Screen name="Join" component={JoinPinScreen} />
-						<Tab.Screen name="Create" component={Create} />
-						<Tab.Screen name="Player" component={Player} />
-						<Tab.Screen name="Choose Team" component={ChooseTeam} />
-						<Tab.Screen name="Profile" component={Profile} />
-						<Tab.Screen name="Statistics" component={Statistics} />
-						<Tab.Screen name="Settings" component={Settings} />
-						<Tab.Screen name="InGame" component={InGame} />
-						<Tab.Screen
-							name="GamePreScreen"
-							component={GamePreScreen}
-						/>
-						<Tab.Screen
-							name="Previous Game"
-							component={PreviousGame}
-						/>
-					</Tab.Navigator>
-					<StatusBar style="auto" />
-				</NavigationContainer>
-			</ImageBackground>
-		);
-	}
+    if (!loggedIn) {
+        return (
+            <ImageBackground
+                source={require("./assets/blurBackground.jpg")}
+                style={styles.image}
+            >
+                <NavigationContainer theme={MyTheme}>
+                    <Tab.Navigator>
+                        <Tab.Screen
+                            name="Login"
+                            component={Login}
+                            options={({ route }) => ({
+                                tabBarVisible: false,
+                            })}
+                        />
+                        <Tab.Screen
+                            name="Register"
+                            component={Register}
+                            options={({ route }) => ({
+                                tabBarVisible: false,
+                            })}
+                        />
+                    </Tab.Navigator>
+                    <StatusBar style="auto" />
+                </NavigationContainer>
+            </ImageBackground>
+        );
+    } else {
+        return (
+            <ImageBackground
+                source={require("./assets/blurBackground.jpg")}
+                style={styles.image}
+            >
+                <NavigationContainer theme={MyTheme}>
+                    <Tab.Navigator tabBar={(props) => <Menu {...props} />}>
+                        <Tab.Screen name="Home" component={HomeScreen} />
+                        <Tab.Screen name="Join" component={JoinPinScreen} />
+                        <Tab.Screen name="Create" component={Create} />
+                        <Tab.Screen name="Player" component={Player} />
+                        <Tab.Screen name="Choose Team" component={ChooseTeam} />
+                        <Tab.Screen name="Profile" component={Profile} />
+                        <Tab.Screen name="Statistics" component={Statistics} />
+                        <Tab.Screen name="Settings" component={Settings} />
+                        <Tab.Screen name="InGame" component={InGame} />
+                        <Tab.Screen
+                            name="GamePreScreen"
+                            component={GamePreScreen}
+                        />
+                        <Tab.Screen
+                            name="Previous Game"
+                            component={PreviousGame}
+                        />
+                    </Tab.Navigator>
+                    <StatusBar style="auto" />
+                </NavigationContainer>
+            </ImageBackground>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	menu: {
-		backgroundColor: "#313334",
-		borderRadius: 10,
-		flexDirection: "row",
-		height: 63,
-		marginHorizontal: 20,
-		marginBottom: 20,
-		color: "#fff",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	image: {
-		flex: 1,
-		resizeMode: "cover",
-		justifyContent: "center",
-	},
+    menu: {
+        backgroundColor: "#313334",
+        borderRadius: 10,
+        flexDirection: "row",
+        height: 63,
+        marginHorizontal: 20,
+        marginBottom: 20,
+        color: "#fff",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+    },
 });
 
 function Menu({ state, descriptors, navigation }) {
-	const notInMenuBar = [
-		"Choose Team",
-		"Profile",
-		"Statistics",
-		"Settings",
-		"InGame",
-		"GamePreScreen",
-		"Previous Game",
-	];
+    const notInMenuBar = [
+        "Choose Team",
+        "Profile",
+        "Statistics",
+        "Settings",
+        "InGame",
+        "GamePreScreen",
+        "Previous Game",
+    ];
 
-	return (
-		<View style={styles.menu}>
-			{state.routes
-				.filter((r) => !notInMenuBar.includes(r.name))
-				.map((route, index) => {
-					const { options } = descriptors[route.key];
-					const label =
-						options.tabBarLabel !== undefined
-							? options.tabBarLabel
-							: options.title !== undefined
-							? options.title
-							: route.name;
+    return (
+        <View style={styles.menu}>
+            {state.routes
+                .filter((r) => !notInMenuBar.includes(r.name))
+                .map((route, index) => {
+                    const { options } = descriptors[route.key];
+                    const label =
+                        options.tabBarLabel !== undefined
+                            ? options.tabBarLabel
+                            : options.title !== undefined
+                            ? options.title
+                            : route.name;
 
-					const isFocused = state.index === index;
+                    const isFocused = state.index === index;
 
-					const onPress = () => {
-						const event = navigation.emit({
-							type: "tabPress",
-							target: route.key,
-						});
+                    const onPress = () => {
+                        const event = navigation.emit({
+                            type: "tabPress",
+                            target: route.key,
+                        });
 
-						if (!isFocused && !event.defaultPrevented) {
-							navigation.navigate(route.name);
-						}
-					};
+                        if (!isFocused && !event.defaultPrevented) {
+                            navigation.navigate(route.name);
+                        }
+                    };
 
-					const onLongPress = () => {
-						navigation.emit({
-							type: "tabLongPress",
-							target: route.key,
-						});
-					};
+                    const onLongPress = () => {
+                        navigation.emit({
+                            type: "tabLongPress",
+                            target: route.key,
+                        });
+                    };
 
-					return (
-						<TouchableOpacity
-							accessibilityRole="button"
-							accessibilityStates={isFocused ? ["selected"] : []}
-							accessibilityLabel={
-								options.tabBarAccessibilityLabel
-							}
-							testID={options.tabBarTestID}
-							onPress={onPress}
-							onLongPress={onLongPress}
-							style={{ flex: 1, alignItems: "center" }}
-						>
-							{route.name === "Home" ? (
-								<Ionicons
-									name={"game-controller-outline"}
-									size={22}
-									color={"#fff"}
-								/>
-							) : null}
-							{route.name === "Join" ? (
-								<Ionicons
-									name={"globe-outline"}
-									size={22}
-									color={"#fff"}
-								/>
-							) : null}
-							{route.name === "Create" ? (
-								<Ionicons
-									name={"add-circle-outline"}
-									size={22}
-									color={"#fff"}
-								/>
-							) : null}
-							{route.name === "Player" ? (
-								<Ionicons
-									name={"person-circle-outline"}
-									size={22}
-									color={"#fff"}
-								/>
-							) : null}
-							<Text
-								style={{ color: isFocused ? "orange" : "#fff" }}
-							>
-								{label}
-							</Text>
-						</TouchableOpacity>
-					);
-				})}
-		</View>
-	);
+                    return (
+                        <TouchableOpacity
+                            accessibilityRole="button"
+                            accessibilityStates={isFocused ? ["selected"] : []}
+                            accessibilityLabel={
+                                options.tabBarAccessibilityLabel
+                            }
+                            testID={options.tabBarTestID}
+                            onPress={onPress}
+                            onLongPress={onLongPress}
+                            style={{ flex: 1, alignItems: "center" }}
+                        >
+                            {route.name === "Home" ? (
+                                <Ionicons
+                                    name={"game-controller-outline"}
+                                    size={22}
+                                    color={"#fff"}
+                                />
+                            ) : null}
+                            {route.name === "Join" ? (
+                                <Ionicons
+                                    name={"globe-outline"}
+                                    size={22}
+                                    color={"#fff"}
+                                />
+                            ) : null}
+                            {route.name === "Create" ? (
+                                <Ionicons
+                                    name={"add-circle-outline"}
+                                    size={22}
+                                    color={"#fff"}
+                                />
+                            ) : null}
+                            {route.name === "Player" ? (
+                                <Ionicons
+                                    name={"person-circle-outline"}
+                                    size={22}
+                                    color={"#fff"}
+                                />
+                            ) : null}
+                            <Text
+                                style={{ color: isFocused ? "orange" : "#fff" }}
+                            >
+                                {label}
+                            </Text>
+                        </TouchableOpacity>
+                    );
+                })}
+        </View>
+    );
 }
--- pages/home/Home.js
+++ pages/home/Home.js
@@ -6,69 +6,69 @@ import GameLogItem from "../../components/homeComponents/GameLogItem";
 import Logo from "../../components/imageComponents/Logo";
 
 const styles = StyleSheet.create({
-	fontWhite: {
-		color: "#fff",
-	},
-	fontWhiteStatusHeader: {
-		color: "#fff",
-		marginLeft: "6%",
-	},
+    fontWhite: {
+        color: "#fff",
+    },
+    fontWhiteStatusHeader: {
+        color: "#fff",
+        marginLeft: "6%",
+    },
 });
 
 function HomeScreen({ navigation }) {
-	const dataMock = [
-		{
-			key: 1,
-			gameType: "Zombie",
-			gameDescription: "Zombie win #7",
-			gameDate: "27.01.21",
-			image: require("../../assets/zombie.png"),
-		},
-		{
-			key: 2,
-			gameType: "Team deathmatch",
-			gameDescription: "Blue team wins",
-			gameDate: "27.01.21",
-			image: require("../../assets/zombie.png"),
-		},
-		{
-			key: 3,
-			gameType: "Capture the flag",
-			gameDescription: "Blue team wins!",
-			gameDate: "22.01.21",
-			image: require("../../assets/ctf.png"),
-		},
-		{
-			key: 4,
-			gameType: "Bomb",
-			gameDescription: "Red team wins",
-			gameDate: "20.08.21",
-			image: require("../../assets/bomb.png"),
-		},
-	];
+    const dataMock = [
+        {
+            key: 1,
+            gameType: "Zombie",
+            gameDescription: "Zombie win #7",
+            gameDate: "27.01.21",
+            image: require("../../assets/zombie.png"),
+        },
+        {
+            key: 2,
+            gameType: "Team deathmatch",
+            gameDescription: "Blue team wins",
+            gameDate: "27.01.21",
+            image: require("../../assets/zombie.png"),
+        },
+        {
+            key: 3,
+            gameType: "Capture the flag",
+            gameDescription: "Blue team wins!",
+            gameDate: "22.01.21",
+            image: require("../../assets/ctf.png"),
+        },
+        {
+            key: 4,
+            gameType: "Bomb",
+            gameDescription: "Red team wins",
+            gameDate: "20.08.21",
+            image: require("../../assets/bomb.png"),
+        },
+    ];
 
-	return (
-		<View>
-			<Logo />
-			<Text style={styles.fontWhiteStatusHeader}>In progress</Text>
-			<ActiveGame navigation={navigation} />
-			<Text style={styles.fontWhiteStatusHeader}>Previous games</Text>
-			<FlatList
-				data={dataMock}
-				keyExtractor={(item, index) => item.key + "a" + index}
-				renderItem={({ item }) => (
-					<GameLogItem
-						key={item.key}
-						gameType={item.gameType}
-						gameDescription={item.gameDescription}
-						gameDate={item.gameDate}
-						image={item.image}
-						navigation={navigation}
-					/>
-				)}
-			/>
-		</View>
-	);
+    return (
+        <View>
+            <Logo />
+            <Text style={styles.fontWhiteStatusHeader}>In progress</Text>
+            <ActiveGame navigation={navigation} />
+            <Text style={styles.fontWhiteStatusHeader}>Previous games</Text>
+            <FlatList
+                data={dataMock}
+                keyExtractor={(item, index) => item.key + "a" + index}
+                renderItem={({ item }) => (
+                    <GameLogItem
+                        key={item.key}
+                        gameType={item.gameType}
+                        gameDescription={item.gameDescription}
+                        gameDate={item.gameDate}
+                        image={item.image}
+                        navigation={navigation}
+                    />
+                )}
+            />
+        </View>
+    );
 }
 
 export default HomeScreen;
